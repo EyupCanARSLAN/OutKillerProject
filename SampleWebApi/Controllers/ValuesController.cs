73ceedd1bf9eba8e326d3b6c5124e548a0bacc6b
CanBey <CanBey@CANBEY>
2018-06-04 23:53:33 +0300

    Function's summeries added and webApi implementation showed

---
+++ SampleWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using ServiceResponse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleWebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public ServiceResult<IEnumerable<string>> Get()
+        {
+            return ServiceResult<IEnumerable<string>>.SuccessResponse(new string[] { "value1", "value2" });
+        }
+
+        // GET api/values/5
+        public ServiceResult<dynamic> Get(int id)
+        {
+            return ServiceResult<dynamic>.FailResponse("Unexpected error occured.");
+        }
+    }
+}
--- ServiceResponse/ServiceResponce.cs
+++ ServiceResponse/ServiceResponce.cs
@@ -1,5 +1,8 @@
 ﻿namespace ServiceResponse
 {
+    /// <summary>
+    /// Status code of the response.
+    /// </summary>
     public enum ServiceResultStatus
     {
         Fail,
@@ -22,11 +25,38 @@
     /// <typeparam name="T"></typeparam>
     public class ServiceResult<T>
     {
+        /// <summary>
+        /// Result status.
+        /// </summary>
         public ServiceResultStatus Status { get; set; }
+        /// <summary>
+        /// Message that can be use when status is fail.
+        /// </summary>
         public string Message { get; set; }
+        /// <summary>
+        /// Data that can be use when status is success to return object.
+        /// </summary>
         public T Data { get; set; }
+        /// <summary>
+        /// Response type for success type.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static ServiceResult<T> SuccessResponse(T data = default(T)) => ServiceResponse(ServiceResultStatus.Success, data);
-        public static ServiceResult<T> FailResponse(string failMessage = null, T failedData = default(T))=> ServiceResponse(ServiceResultStatus.Fail, failedData, failMessage);
+        /// <summary>
+        /// Response type for fail type.
+        /// </summary>
+        /// <param name="failMessage">Progress fail reason can be explained as string</param>
+        /// <param name="failedData">Returning object when progress fail.</param>
+        /// <returns></returns>
+        public static ServiceResult<T> FailResponse(string failMessage = null, T failedData = default(T)) => ServiceResponse(ServiceResultStatus.Fail, failedData, failMessage);
+        /// <summary>
+        /// A body of Service Result that calling from FailResponse and SuccessResponse
+        /// </summary>
+        /// <param name="serviceResult">Status of result</param>
+        /// <param name="data">Returning object as result item</param>
+        /// <param name="message"></param>
+        /// <returns></returns>
         private static ServiceResult<T> ServiceResponse(ServiceResultStatus serviceResult, T data = default(T), string message = null) => new ServiceResult<T>
         {
             Status = serviceResult,
